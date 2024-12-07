96fe97a5952b93a5cdb0b743e3359a9092d80ed4
chainvisions <chainvisions@outlook.com>
2024-12-07 15:27:51 -0700

    feat: Break ground for HoneyBadgerBFT consensus support

---
+++ src/base/hbbft/BaseHBBTBlockRewards.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+/// @title Block Rewards Base (HBBT)
+/// @author Chainvisions
+/// @notice Base POSDAO block rewards contract for the HBBFT (HoneyBadger BFT) consensus mechanism.
+
+contract BaseHBBTBlockRewards {}
